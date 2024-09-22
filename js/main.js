7bb10e9784c77d41219d864c8df94d16af99b9df
abdullahsofte <abdullah.softe@gmail.com>
2024-09-22 21:29:24 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+document.getElementById('btn-login').addEventListener('click', function(event){
+    event.preventDefault();
+  const phoneNumber = document.getElementById('phone-number').value;
+
+  const password = document.getElementById('password').value;
+  console.log(password);
+  
+    
+})
