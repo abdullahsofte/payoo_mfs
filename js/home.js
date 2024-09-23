901f912a19abca44b3fc60e6ee82b54ae90db0b9
abdullahsofte <abdullah.softe@gmail.com>
2024-09-23 22:00:50 +0600

    add money complete

--- js/home.js
+++ js/home.js
@@ -1,8 +1,22 @@
-    // add money functionality
-document.getElementById('btn-add-money').addEventListener('click', function(event){
+// add money functionality
+document.getElementById('btn-add-money').addEventListener('click', function (event) {
     event.preventDefault();
-   const pinNUmber = document.getElementById('pin_number').value;
-   const addAmount = document.getElementById('add_amount').value;
-    console.log(pinNUmber, addAmount);
-    
+    const pinNUmber = document.getElementById('pin_number').value;
+    const addAmount = document.getElementById('add_amount').value;
+    if (pinNUmber === '123') {
+        const currentBalance = document.getElementById('balance-amount').innerText;
+        const addMoneyNumber = parseFloat(addAmount);
+        const currentMoneyNumber = parseFloat(currentBalance);
+
+        const newBalance = addMoneyNumber + currentMoneyNumber;
+        // console.log(newBalance);
+
+        document.getElementById('balance-amount').innerText = newBalance;
+
+
+
+    } else {
+        alert('Failed to add money ! please try again');
+    }
+
 });
